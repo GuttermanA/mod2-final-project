9d57c1ff9929dc8b0638fe80576d9698bd9b4436
GuttermanA <alexanderf.gutterman@gmail.com>
2018-02-14 15:12:23 -0500

    completed automatic db seeding

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,26 +7,6 @@ class Question < ApplicationRecord
 
   validate :valid_choices, :unique_choice_combo
 
-  def self.generate_all_questions
-    Category.all.each do |category|
-      id_arr = category.choices.map {|choice| choice.id}
-      id_combo_arr = id_arr.combination(2).to_a
-      id_combo_arr.each do |id_combo|
-        q = self.new(category_id: category.id)
-        if category.name.downcase.include?('nsfw')
-          q.nsfw_flag = true
-        end
-        choice_a = Choice.find(id_combo[0])
-        choice_b = Choice.find(id_combo[1])
-        q.description = "#{choice_a.name} or #{choice_b.name}"
-        q.choices << choice_a
-        q.choices << choice_b
-        q.save
-        puts "Generated #{id_combo_arr.size} questions from #{category.choices.size} choices for #{category.name}"
-      end
-    end
-  end
-
   #validations
   def valid_choices
     if self.choices.size != 2
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,29 @@
+# each question category should be broken into it's own .csv for the seed to run correctly.
+
 seed_dir = './db/seeds/'
 
-Dir.foreach(seed_dir) do |item|
+Dir.glob('./db/seeds/*.csv') do |item|
   next if item == '.' or item == '..'
-  csv_text = File.read("#{seed_dir}#{item}")
+
+  csv_text = File.read("#{item}")
   csv = CSV.parse(csv_text, :headers => true)
   csv_arr = []
   csv.each do |row|
     csv_arr << row.to_hash
   end
-
-  cat = Category.find_or_create_by(name: csv_arr[0]["category"])
+  puts "Read #{item}"
+  next if Category.find_by(name: csv_arr[0]["category"])
+  created_category = Category.find_or_create_by(name: csv_arr[0]["category"])
   csv_arr.each do |row|
     c = Choice.new
     c.name = row["name"]
     c.content = row["content"]
     c.img_url = row["img_url"]
-    c.category = cat
+    c.category = created_category
     c.save
   end
-  Question.generate_all_questions
-  puts "completed #{item}"
+  created_category.generate_all_questions_for_category
+  puts "Completed #{item}"
 end
 
-# csv_text = File.read("db/seeds/WYR.csv")
-# csv = CSV.parse(csv_text, :headers => true)
-# csv_arr = []
-# csv.each do |row|
-#   csv_arr << row.to_hash
-# end
-#
-#
-# cat = Category.find_or_create_by(name: csv_arr[0]["category"])
-#
-# csv_arr.each do |row|
-#   c = Choice.new
-#   c.name = row["name"]
-#   c.content = row["content"]
-#   c.img_url = row["img_url"]
-#   c.category = cat
-#   c.save
-# end
-
-# Question.generate_all_questions
+puts "Done. Generated #{Question.all.size} questions."
